328599b737bbcbce050125f0984615961875217a
jet981217 <jet981217@naver.com>
2023-05-03 23:56:38 +0900

    fix:Fix code review

--- models/baseline/baseline.py
+++ models/baseline/baseline.py
@@ -10,11 +10,10 @@ class TextToImageBaseline(nn.Module):
         self.decoder = decoder
         
     def forward(self, text, noise):
-        encoded_text = self.encoder(text).last_hidden_state.mean(dim=1)
+        encoded_text = self.encoder(text).last_hidden_state[:,0]
         mlp_output = self.mlp(encoded_text)
-        print(mlp_output.size())
+        
         mlp_output = mlp_output.view(-1, 100, 1, 1)
         decoder_input = torch.sum(torch.stack([mlp_output, noise], dim=0), dim=0)
         generated_image = self.decoder(decoder_input)
         return generated_image
-
