90e7f2340825639d0180ee13dd8389f5a8cd474e
jet981217 <jet981217@naver.com>
2023-05-14 10:57:27 +0900

    feat: Add stable diffusion mapper

--- models/map_model.py
+++ models/map_model.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn.functional as F
 
 from .baseline import GANDecoder, MLP, TextToImageBaseline
@@ -10,3 +11,17 @@ def map_model(model_type):
         tokenizer = ElectraTokenizer.from_pretrained("google/electra-small-discriminator")
 
         return TextToImageBaseline(encoder, MLP(256, 512, 100), GANDecoder(1, 100, 64, 3)), tokenizer
+    if model_type in ["baseline-sd", "baseline-stable_diffusion"]:
+        """
+        Beware that this returns pipeline not torch module
+        """
+        from diffusers import StableDiffusionPipeline, EulerAncestralDiscreteScheduler
+    
+        repo = "Bingsu/my-korean-stable-diffusion-v1-5"
+        euler_ancestral_scheduler = EulerAncestralDiscreteScheduler.from_config(repo, subfolder="scheduler")
+        pipe = StableDiffusionPipeline.from_pretrained(
+            repo, scheduler=euler_ancestral_scheduler, torch_dtype=torch.float16,
+        )
+        pipe.to("cuda" if torch.cuda.is_available() else "cpu")
+
+        return pipe
